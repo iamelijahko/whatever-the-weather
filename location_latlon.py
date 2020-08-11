f1df8d8d6dc74f21b233b450c22ecc7ae5772580
Elijah Ko <57324395+elijahkogithub@users.noreply.github.com>
2020-08-11 15:31:16 +0100

    Delete location_latlon.py

--- location_latlon.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent='location_application')
-location = geolocator.geocode("paris")
-print(location.address)
-print((location.latitude, location.longitude))
